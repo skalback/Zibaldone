ee4ead724e68338418b965268b0e72ea37dc9747
skalback <70301513+skalback@users.noreply.github.com>
2021-02-02 19:24:36 -0500

    compression

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 const express = require("express");
+var compression = require('compression')
+var express = require('express')
+ 
+var app = express()
+app.use(compression({ filter: shouldCompress }))
+ 
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+ 
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
 
 // Initialize application on port 8080
 const app = express();
